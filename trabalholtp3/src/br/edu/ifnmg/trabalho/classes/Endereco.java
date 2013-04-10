d2f20777d764393007befd58cfd2f1baa8565de8
emerson <emerson@emerson-System-Product-Name>
2013-04-10 13:49:07 -0300

    Criação da classe Pessoa

---
+++ trabalholtp3/src/br/edu/ifnmg/trabalho/classes/Endereco.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.trabalho.classes;
+
+/**
+ *
+ * @author emerson
+ */
+public class Endereco {
+    private int Id;
+    private String Rua;
+    private int Num;
+    private String Bairro;
+    
+    public Endereco(){
+    
+    }
+  
+}
