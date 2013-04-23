11ad4e7e83df011361c9af9198d601ad795e6519
aluno <aluno@aluno-desktop>
2013-04-23 08:21:52 -0300

    construção BD

---
+++ trabalholtp3/src/br/edu/ifnmg/trabalho/DataAccess/Bd.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.trabalho.DataAccess;
+
+/**
+ *
+ * @author aluno
+ */
+public class Bd { 
+    private static String classconexao = "jdbc:mysql://localhost:3306/teste";
+    
+}
