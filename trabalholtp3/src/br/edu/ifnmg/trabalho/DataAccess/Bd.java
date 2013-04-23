cf830cd0507c0f0510430e9dd4ed594bd08877b4
aluno <aluno@aluno-desktop>
2013-04-23 08:47:07 -0300

    aula terça-feira

--- trabalholtp3/src/br/edu/ifnmg/trabalho/DataAccess/Bd.java
+++ trabalholtp3/src/br/edu/ifnmg/trabalho/DataAccess/Bd.java
@@ -4,11 +4,45 @@
  */
 package br.edu.ifnmg.trabalho.DataAccess;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author aluno
  */
 public class Bd { 
-    private static String classconexao = "jdbc:mysql://localhost:3306/teste";
+    private Connection Conexao;
+    
+    //----
+    
+    public Connection getConexao(){
+        return Conexao;
+    }
+    
+    public void setConexao(Connection Conexao){
+        this.Conexao = Conexao;
+    }
+    
+    public Bd(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Conexao = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/teste","root","aluno");
+        
+        }catch(ClassNotFoundException ex){
+            Logger.getLogger(Bd.class.getName()).log(Level.SEVERE, null, ex); 
+        }catch(SQLException ex){
+             Logger.getLogger(Bd.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    
+    }
+   
+
+   
+
     
 }
